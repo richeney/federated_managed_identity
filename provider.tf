2f82c0381263af69b394773e73dc3eb99827ca93
Richard Cheney <richeney@microsoft.com>
2022-11-10 17:28:48 +0000

    Go again...

--- provider.tf
+++ provider.tf
@@ -11,13 +11,25 @@ terraform {
 provider "azurerm" {
   features {}
 
-  tenant_id       = "3c584bbd-915f-4c70-9f2e-7217983f22f6"
+  // tenant_id       = "3c584bbd-915f-4c70-9f2e-7217983f22f6"
   // subscription_id = "9b7a166a-267f-45a5-b480-7a04cfc1edf6"
-  client_id       = "8fbf5ac1-d1e4-47a3-8f6a-cb9e9b72ab0e"
+  // client_id       = "8fbf5ac1-d1e4-47a3-8f6a-cb9e9b72ab0e"
 
-  use_oidc           = true
+  use_oidc = true
   //oidc_request_token = var.oidc_request_token
   //oidc_request_url   = var.oidc_request_url
 
   storage_use_azuread = true
+
+  backend "azurerm" {
+    use_oidc = true
+
+    tenant_id            = "3c584bbd-915f-4c70-9f2e-7217983f22f6"
+    subscription_id      = "9b7a166a-267f-45a5-b480-7a04cfc1edf6"
+    resource_group_name  = "terraform"
+    storage_account_name = "terraform66615a0f"
+
+    container_name = "tfstate"
+    key            = "federated_managed_identity"
+  }
 }
