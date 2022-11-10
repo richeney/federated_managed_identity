ed981f13c2b4ef83b6e22976bed1df4d811bea45
Richard Cheney <richeney@microsoft.com>
2022-11-10 17:10:02 +0000

    Reduce arguments

--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
-variable "oidc_request_token" {}
-variable "oidc_request_url" {}
 
 terraform {
   required_providers {
@@ -13,13 +11,13 @@ terraform {
 provider "azurerm" {
   features {}
 
-  tenant_id       = "3c584bbd-915f-4c70-9f2e-7217983f22f6"
-  subscription_id = "9b7a166a-267f-45a5-b480-7a04cfc1edf6"
-  client_id       = "8fbf5ac1-d1e4-47a3-8f6a-cb9e9b72ab0e"
+  // tenant_id       = "3c584bbd-915f-4c70-9f2e-7217983f22f6"
+  // subscription_id = "9b7a166a-267f-45a5-b480-7a04cfc1edf6"
+  // client_id       = "8fbf5ac1-d1e4-47a3-8f6a-cb9e9b72ab0e"
 
   use_oidc           = true
-  oidc_request_token = var.oidc_request_token
-  oidc_request_url   = var.oidc_request_url
+  //oidc_request_token = var.oidc_request_token
+  //oidc_request_url   = var.oidc_request_url
 
   storage_use_azuread = true
 }
